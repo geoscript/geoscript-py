77a5e9cb7ea0bba64a128a12cab0a7071c20cb20
jdeolive <jdeolive@gmail.com>
2009-08-13 13:47:18 -0600

    added doc tests for geom module

--- geoscript/geom.py
+++ geoscript/geom.py
@@ -1,3 +1,21 @@
+"""
+geom module -- Provides utilities for the construction and manipulation of 
+geometry objects.
+
+Geometry constructors take lists of lists defining the coordinates which make
+up the geometry.
+
+>>> line = LineString([[1,2],[3,4]])
+>>> str(line)
+'LINESTRING (1 2, 3 4)'
+
+Constructors also take lists of tuples.
+
+>>> line = LineString([(1,2),(3,4)])
+>>> str(line)
+'LINESTRING (1 2, 3 4)'
+"""
+
 from java.lang import Double
 from com.vividsolutions.jts.geom import Coordinate, GeometryFactory
 from com.vividsolutions.jts.io import WKTReader
@@ -7,10 +25,57 @@ from org.geotools.referencing import CRS
 _gf = GeometryFactory()
 _wktreader = WKTReader()
 
-def Point(x,y,z=Double.NaN):
-  return _gf.createPoint(Coordinate(x,y,z))
+def Point(x,y=Double.NaN,z=Double.NaN):
+  """
+  Constructs a point geometry.
+
+  This function accepts a variety of inputs. The first being direct x,y,z
+  arguments:
+
+  >>> point = Point(1,2)
+  >>> str(point)
+  'POINT (1 2)'
+
+  It also accepts a list or tuple of x,y,z:
+
+  >>> point = Point([1,2])
+  >>> str(point)
+  'POINT (1 2)'
+
+  Or a list of lists or tuples:
+
+  >>> point = Point([[1,2]])
+  >>> str(point)
+  'POINT (1 2)'
+  """
+
+  if type(x) in (tuple,list) and Double.isNaN(y):
+    coords = x
+
+    if len(coords) == 1 and type(coords[0]) in (tuple,list):
+      coords = coords[0]
+
+    c = Coordinate(coords[0],coords[1])
+    if len(coords) > 2:
+      c.z = coords[2]
+
+  else:
+     c = Coordinate(x,y)
+     c.z = z
+
+  return _gf.createPoint(c)
 
 def LineString(coords):
+  """
+  Constructs a linestring geometry.
+
+  This function takes a list of lists or tuples:
+
+  >>> line = LineString([ [1,2],[3,4] ])
+  >>> str(line)
+  'LINESTRING (1 2, 3 4)'
+  """
+
   l = []
   for c in coords:
     l.append( Coordinate(c[0],c[1]) )
@@ -23,18 +88,56 @@ def LineString(coords):
     return _gf.createLineString(l)
 
 def Polygon(ring,holes=None):
+
+  """
+  Constructs a polygon geometry.
+
+  The first argument of this function is a list of lists defining the outer
+  ring of the polygon:
+  
+  >>> poly = Polygon([ [1,2],[3,4],[5,6],[1,2] ])
+  >>> str(poly)
+  'POLYGON ((1 2, 3 4, 5 6, 1 2))'
+  
+  The second argument is optional and defines a list of lists of lists defining
+  the inner rings or holes of the polygon 
+  >>> exterior = [ [-10,-10],[10,-10],[10,10],[-10,10],[-10,-10] ]
+  >>> holes = [ [ [-5,-5],[-1,-5],[-3,-2],[-5,-5] ], [ [5,5],[9,5],[7,7],[5,5] ] ]
+  >>> poly = Polygon(exterior,holes)
+  >>> str(poly)
+  'POLYGON ((-10 -10, 10 -10, 10 10, -10 10, -10 -10), (-5 -5, -1 -5, -3 -2, -5 -5), (5 5, 9 5, 7 7, 5 5))'
+  """
+
   outer = LineString(ring)
   inner = []
   if holes:
     for h in holes:
-      inner.append(_gf.linestring(h))
+      inner.append(LineString(h))
 
   return _gf.createPolygon(outer,inner)
 
 def Geometry(wkt):
+  """
+  Constructs a geometry from well known text.
+
+  >>> g = Geometry('POINT (1 2)')
+  >>> str(g)
+  'POINT (1 2)'
+  """
   return _wktreader.read(wkt)
 
 def reproject(g, fromsrs, tosrs):
+  """
+  Reprojects a geometry from a source project to a target projection. 
+
+  The arguments 'fromsrs' and 'tosrs' are specified as epsg codes.
+
+  >>> p1 = Point(-125,50)
+  >>> p2 = reproject(p1,'epsg:4326','epsg:3005')
+  >>> str(p2)
+  'POINT (1071693.1296328472 554289.941892416)'
+  """
+
   fromcrs = CRS.decode(fromsrs)
   tocrs = CRS.decode(tosrs)
   tx = CRS.findMathTransform(fromcrs,tocrs)
@@ -42,4 +145,3 @@ def reproject(g, fromsrs, tosrs):
   gt.mathTransform = tx
 
   return gt.transform(g)
-
--- tests/runall.py
+++ tests/runall.py
@@ -4,9 +4,12 @@ from os import path
 sys.path.insert(0,path.join(path.dirname(__file__),path.pardir))
 
 import unittest
-import geom
+import doctest
+import geomtest
+from geoscript import geom
 
 loader = unittest.TestLoader()
 runner = unittest.TextTestRunner(verbosity=2)
 
-runner.run(loader.loadTestsFromTestCase(geom.GeomTest))
+runner.run(doctest.DocTestSuite(geom))
+runner.run(loader.loadTestsFromTestCase(geomtest.GeomTest))
