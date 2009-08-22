c3791376fd8bd970a686e671a231c51ad146fed9
jdeolive <jdeolive@gmail.com>
2009-08-22 16:03:15 -0700

    moved reproject function to a dedicated proj module

--- tests/runall.py
+++ tests/runall.py
@@ -6,10 +6,13 @@ sys.path.insert(0,path.join(path.dirname(__file__),path.pardir))
 import unittest
 import doctest
 import geomtest
-from geoscript import geom
+import projtest
+from geoscript import geom, proj
 
 loader = unittest.TestLoader()
 runner = unittest.TextTestRunner(verbosity=2)
 
 runner.run(doctest.DocTestSuite(geom))
 runner.run(loader.loadTestsFromTestCase(geomtest.GeomTest))
+runner.run(doctest.DocTestSuite(proj))
+runner.run(loader.loadTestsFromTestCase(projtest.ProjTest))
