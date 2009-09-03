7825b18fa45a9915d90c22b42ab2aed9c29b4486
jdeolive <jdeolive@gmail.com>
2009-09-02 18:35:52 -0600

    changes to feature module to have feature fully delegate to gt feature

--- geoscript/feature.py
+++ geoscript/feature.py
@@ -2,148 +2,281 @@
 feature module -- Provides classes for simple feature access.
 """
 
-from com.vividsolutions.jts.geom import Geometry
+import java
+import string
+from geoscript import geom, proj
 from org.opengis.feature.type import GeometryDescriptor
-from org.geotools.feature.simple import SimpleFeatureBuilder
-from org.geotools.referencing import CRS 
+from org.geotools.feature import NameImpl
+from org.geotools.feature.simple import SimpleFeatureBuilder, SimpleFeatureTypeBuilder
+
+_tmap = {}
+_tmap[int] = java.lang.Integer
+_tmap[java.lang.Integer] = int
+_tmap[java.lang.Short] = int
+_tmap[java.lang.Float] = int
+
+_tmap[long] = java.lang.Long
+_tmap[java.lang.Long] = long
+
+_tmap[str] = java.lang.String
+_tmap[unicode] = java.lang.String
+_tmap[java.lang.String] = str
+
+_tmap[float] = java.lang.Double
+_tmap[java.lang.Double] = float
+_tmap[java.lang.Float] = float
 
 """
-A feature type is the schema for a feature. It specifies the names and
-types of a features attributes.
- 
->>> atts = []
->>> atts.append({'name':'att1', 'type', int})
->>> atts.append({'name':'att2', 'type', str})
->>> ft = FeatureType(atts=atts)
->>> str(ft)
-"{'att1': int, 'att2': str}"
+Maps a jython type to its associated java type.
 """
+def _map(o):
+  if isinstance(o,type):
+    t = o 
+  else:
+    t = type(o)
+
+  if _tmap.has_key(t):
+    mapped = _tmap[t]
+    if isinstance(o,type):
+      return mapped
+    else:
+      return mapped(o)
+
+  return o
+
 class FeatureType:
+  """
+  A feature type is the schema for a feature. It specifies the names and 
+  types of a features attributes. A feature type can be constructed from a 
+  list of name, type tuples
+   
+  >>> import geom
+  >>> ft = FeatureType('myFeatureType', [ ('geom',geom.Point), ('att1',int), ('att2',str) ])
+  >>> str(ft)
+  'myFeatureType {geom: Point, att1: int, att2: str}'
 
-  def __init__(self, name=None, ft=None, atts={}, geom={}):
-    self.name = name
+  """
+
+  def __init__(self, name=None, atts=[], ft=None):
     self.ft = ft
-    self.atts = atts
-    self.geom = geom
 
-    #set a name if not supplied directly
-    if not self.name and ft:
-      self.name = ft.localName
-    
-    if ft:
-      #generate attributes from gt feature type
-      for ad in ft.attributeDescriptors:
-         att = {}
-         att['name'] = ad.localName
-         att['type'] = ad.type.binding
-         if isinstance(ad,GeometryDescriptor):
-            #geom attribute, look for a crs
-            crs = ad.type.coordinateReferenceSystem
-            if crs:
-              att['srs'] = CRS.toSRS(crs)
-              att['epsg'] = CRS.lookupEpsgCode(crs,True)
+    if name and atts:
+      # name and attributes specified directly, generate gt feature type 
+      # from list
+      tb = SimpleFeatureTypeBuilder()
+      tb.setName(NameImpl(name))
+      for att in atts:
+        name, typ = att[0], att[1]
+        if issubclass(typ, geom.Geometry):
+          # look for srs/crs info
+          if len(att) > 2:
+            xrs = att[2]
+            if isinstance(xrs,str):
+              tb.srs(xrs)
+            elif isinstance(xrs,proj.CRS):
+              tb.crs(xrs)
+          
+        # we call _map() here to avoid setting the type binding to a Python
+        # (eg: PyInteger) type, but rather a native java type (Integer)
+        tb.add(name, _map(typ))
 
-         if isinstance(ad,GeometryDescriptor) and not self.geom:
-            #default geom attribute
-            self.geom = att
-         else:    
-            #regular attribute
-            self.atts[att['name']] = att
+      self.ft = tb.buildFeatureType()
+        
+    elif ft:
+      # gt feature type specified directly
+      self.ft = ft
+    else:
+      raise Exception('No attributes specified for feature type.')
 
-    elif atts:
-      #attributes supplied directly, scan for geometry
-      for att in atts.values():
-        if isinstance(att['type'],Geometry) and not self.geom:
-           self.geom = att
+  def name(self):
+    """
+    The name of the feature type.
 
-      if self.geom:
-        atts.remove(self.geom)
+    >>> ft = FeatureType('myFeatureType', [ ('geom',geom.Point), ('att1',int), ('att2',str) ])
+    >>> str(ft.name())
+    'myFeatureType'
+    """
+
+    return self.ft.name.localPart
+
+  def geom(self):
+    """
+    The geometry attribute of the feature type. The attribute is represented by
+    a tuple of the form (<name>,<type> [,<crs>])
+
+    >>> ft = FeatureType('myFeatureType', [ ('geom',geom.Point), ('att1',int), ('att2',str) ])
+    >>> g = ft.geom()
+    >>> str(g[0])
+    'geom'
+    >>> g[1].__name__
+    'Point'
+    """
+
+    gd = self.ft.geometryDescriptor
+    if gd:
+      geom = (gd.localName,gd.type.binding)
+      if gd.coordinateReferenceSystem:
+        geom = (geom[0],geom[1],gd.coordinateReferenceSystem)
+
+      return geom
+
+  def atts(self):
+    """
+    Iterates over attributes.
+
+    >>> ft = FeatureType('myFeatureType', [ ('geom',geom.Point), ('att1',int), ('att2',str) ])
+    >>> str(', '.join(['%s: %s' % (att,typ.__name__) for att,typ in ft.atts()]))
+    'geom: Point, att1: int, att2: str'
+    """
+
+    for ad in self.ft.attributeDescriptors:
+      yield (ad.localName, _map(ad.type.binding))
+
+  def attnames(self):
+    """ 
+    Iterates over attribute names.
+    >>> ft = FeatureType('myFeatureType', [ ('geom',geom.Point), ('att1',int), ('att2',str) ])
+    >>> str(', '.join(['%s' % (att) for att in ft.attnames()]))
+    'geom, att1, att2'
+    """
+
+    for ad in self.ft.attributeDescriptors:
+      yield ad.localName
+
+  def feature(self, vals, id=None):
+    """
+    Creates a feature of the type from a list of attribute values.
+
+    >>> ft = FeatureType('myFeatureType', [ ('geom',geom.Point), ('att1',int), ('att2',str) ])
+    >>> f = ft.feature([geom.point(1,1), 1, 'one'], 'fid1')
+    >>> str(f) 
+    'fid1 {geom: POINT (1 1), att1: 1, att2: one}'
+    """
+    return Feature(zip([att for att in self.attnames()],vals), id, self)
+     
+  def __str__(self):
+    atts = ['%s: %s' % (att,typ.__name__) for att,typ in self.atts()]
+    return '%s {%s}' % (self.name(), string.join(atts,', '))
 
 class Feature:
   """
   A feature is a set of attributes (key value pairs) and a geometry.
 
   >>> import geom
-  >>> f = Feature(atts={'name':'widget'},geom=geom.Point(1,2))
+  >>> f = Feature([ ('name','widget'), ('geom', geom.point(1,2)) ], 'fid1') 
   >>> str(f)
-  "{'name': 'widget'}; POINT (1 2)"
+  'fid1 {name: widget, geom: POINT (1 2)}'
 
   The attributes of a feature can be get and set:
 
-  >>> f.get('name')
+  >>> str(f.get('name'))
   'widget'
   >>> f.set('name','foobar')
-  >>> f.get('name')
+  >>> str(f.get('name'))
   'foobar'
 
   The geometry of a feature can also be modified:
 
-  >>> f.geom = geom.LineString([ [1,2],[3,4] ])
-  >>> str(f.geom)
-  'LINESTRING (1 2, 3 4)'
+  >>> f.geom()
+  POINT (1 2)
+  >>> f.geom(geom.point(3,4))
+  POINT (3 4)
 
   """
-  def __init__(self, id=None, ftype=None, f=None, atts=None, geom=None):
-    self.id = id
-    self.ftype = ftype
-    self.f = f
-    self.atts = {}
-    self.geom = None
-    
-    # set a unique identifier
-    if not id:
-      if f:
-        # set from feature
-        self.id = f.id
-      else:
-        # generate a unique id
-        import uuid
-        self.id = str(uuid.uuid1())
-
-        # qualify with feature type name if possible
-        if ftype and ftype.name:
-          self.id = '%s.%s' % (ftype.name, self.id)
-
-    if ftype and f:
-       for att in ftype.atts.keys():
-         if ftype.geom and att == ftype.geom['name']:
-            self.geom = f.getAttribute(att)
-         else:
-            self.atts[att] = f.getAttribute(att)
-
-    if not ftype:
-       #generate type from attributes
-       if atts:
-         tatts = {}
-         for att,val in atts.iteritems():
-           tatts[att] = {'name':att, 'type':type(val)}
-
-         self.ftype = FeatureType(atts=tatts)
+  def __init__(self, atts=None, id=None, ftype=None, f=None):
 
     if atts:
-       for att,val in atts.iteritems():
-           self.atts[att] = val
+      # attributes specified directly
 
-    if geom:
-       self.geom = geom
-       
-  def __str__(self):
-    return '%s; %s' % (str(self.atts),str(self.geom))
+      # turn into list of tuples if necessary
+      if isinstance(atts, dict):
+        l = [(x,y) for x,y in atts.iteritems()]
+        atts = l
 
-  def set(self,att,value):
-     self.atts[att] = value
+      # generate feature type if necessary
+      self.ftype = ftype 
+      if not self.ftype: 
+        self.ftype = FeatureType('feature', [(x[0],type(x[1])) for x in atts])
+      
+      # generate feature
+      b = SimpleFeatureBuilder(self.ftype.ft)
+      for att in atts: 
+        b.set(att[0],att[1])
+
+      self.f = b.buildFeature(id)
+
+    elif f:
+      # feature specififed directly
+      self.f = f
+      self.ftype = ftype if ftype else FeatureType(f.type)
+
+    else:
+      raise Exception('No attributes specified for feature')
+
+  def id(self):
+    """
+    Id of the feature.
+
+    >>> import geom
+    >>> f = Feature({'geom': geom.point(1,1)}, 'fid.one')
+    >>> str(f.id())
+    'fid.one'
+    """
+
+    return self.f.identifier.toString()
+
+  def geom(self, g=None):
+    """
+    Gets and sets the geometry of the feature.
+    >>> import geom
+    >>> f = Feature({'geom': geom.point(1,1)}, 'fid.one')
+    >>> f.geom()
+    POINT (1 1)
+    >>> f.geom(geom.point(2,2))
+    POINT (2 2)
+    """
+
+    if g:
+      self.f.defaultGeometry = g
+
+    return self.f.defaultGeometry
 
   def get(self,att):
-     if self.atts.has_key(att): 
-        return self.atts[att]
+    """
+    Gets an attribute of a feature.
+    >>> import geom
+    >>> f = Feature({'geom': geom.point(1,1), 'name': 'one'}, 'fid.one')
+    >>> str(f.get('name'))
+    'one'
+    """
+    return self.f.getAttribute(att)
 
-  def _sync(self):
-     if not self.f:
-       b = SimpleFeatureBuilder(self.type.ft) 
-       self.f = b.buildFeature(None,[])
+  def set(self,att,value):
+    """
+    Sets an attribute of a feature.
+    >>> import geom
+    >>> f = Feature({'geom': geom.point(1,1), 'name': 'one'}, 'fid.one')
+    >>> str(f.get('name'))
+    'one'
+    >>> f.set('name', 'two')
+    >>> str(f.get('name'))
+    'two'
+    """
 
-     for att, val in self.atts.iteritems(): 
-        self.f.setAttribute(att,val)
+    self.f.setAttribute(att,value)
 
-     self.f.setDefaultGeometry(self.geom)
- 
+  def atts(self):
+    """
+    Iterates over attributes.
+    >>> import geom
+    >>> f = Feature({'geom': geom.point(1,1), 'name': 'one'}, 'fid.one')
+    >>> str(', '.join(['%s: %s' % (att,val) for att,val in f.atts()]))
+    'geom: POINT (1 1), name: one'
+    """
+
+    for att in self.ftype.attnames():
+      yield (att, _map(self.f.getAttribute(att)))
+
+  def __str__(self):
+    atts = ['%s: %s' % (att,val) for att,val in self.atts()]
+    return '%s {%s}' % (self.id(), string.join(atts,', '))
--- tests/featuretest.py
+++ tests/featuretest.py
@@ -1,13 +1,31 @@
 import unittest
-from geoscript import geom, feature
+from geoscript import geom, proj, feature
 
 class FeatureTest(unittest.TestCase):
 
+  def testFeatureTypeBasic(self):
+    atts = [('att1',str),('att2',int),('geom',geom.Point)]
+    ft = feature.FeatureType('test',atts)
+
+    self.assertEqual('test',ft.name())
+    n = 0
+    for att,typ in ft.atts():    
+      self.assertEqual(atts[n][0], att)
+      self.assertEqual(atts[n][1], typ)
+      n = n+1 
+
+    self.assertEqual(len(atts), n)
+    self.assertEqual(atts[2],ft.geom())
+
+  def testFeatureTypeGeomSRS(self):
+    ft = feature.FeatureType('test',[('att1',str),('att2',int),('geom',geom.Point,'epsg:3005')])
+    self.assert_(isinstance(ft.geom()[2], proj.CRS))
+    self.assertEqual(proj.crs.decode('epsg:3005'),ft.geom()[2])
+
   def testBasic(self):
     id = 'fid'
-    g = geom.Point(-125,50)
-    a = {'x':1, 'y': 1.1, 'z': 'one'}
-    f = feature.Feature(id='fid', atts=a, geom=g)
-    self.assertEqual(id,f.id)
-    self.assertEqual(g,f.geom)
-    self.assertEqual(a,f.atts)
+    g = geom.point(-125,50)
+    a = {'x': 1, 'y': 1.1, 'z': 'one', 'geom': g}
+    f = feature.Feature(a,'fid')
+    self.assertEqual(id,f.id())
+    self.assertEqual(g,f.geom())
