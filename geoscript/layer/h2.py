8c674dc2013e5d27b140e042d7306100ef2648bf
jdeolive <jdeolive@gmail.com>
2009-09-11 14:35:42 -0400

    overiding add() method for h2 layer in order to properly set crs

--- geoscript/layer/h2.py
+++ geoscript/layer/h2.py
@@ -2,6 +2,7 @@
 layer.h2 module -- H2 implementation of Layer.
 """
 
+from geoscript import geom, feature
 from geoscript.layer import Layer
 from org.geotools.data.h2 import H2DataStoreFactory
 
@@ -15,3 +16,15 @@ class H2Layer(Layer):
     
     fs = h2.getFeatureSource(table)
     Layer.__init__(self, fs)
+
+  def add(self, o):
+    # h2 requires crs to be set in geometry user data
+    if self.crs: 
+      if isinstance(o, feature.Feature):
+        o.geom().userData = self.crs
+      elif isinstance(o, list):
+        for att,val in o:
+          if isinstnace(val, geom.Geometry):
+            val.userData = self.crs
+
+    Layer.add(self,o)
