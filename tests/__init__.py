2d9eafc548d849a48b06527e6e3da92be40b4879
Justin Deoliveira <jdeolive@justin-deoliveiras-macbook.local>
2009-09-30 12:36:41 -0400

    storing h2 test data as zip and extracting on test setup

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,23 @@
+import os
+from zipfile import ZipFile
 from dbexts import dbexts
+
+# unzip data into work
+if not os.path.exists('work'):
+  os.mkdir('work')
+
+zip = ZipFile('data/states.db.zip')
+for file in zip.namelist():
+  target = os.path.join('work',file)
+  if os.path.exists(target):
+     continue
+
+  if file.endswith('/'):
+     os.mkdir(target)
+  else:
+     f = open(os.path.join('work',file), 'w')
+     f.write(zip.read(file))
+     f.close()
 
 # init h2 database
 db = dbexts('h2', 'dbexts.ini')
