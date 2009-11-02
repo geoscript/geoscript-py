d0e825290baa998a1211a9f597b8b917398d0a66
jdeolive <jdeolive@192-168-100-47.tpgi.com.au>
2009-11-02 13:51:14 +1000

    removed packaging of geotools libs from setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import os
 from distutils.core import setup
 from setuptools import find_packages
 
-geotools_libs = ['lib/target/%s' % (f) for f in os.listdir('lib/target')]
 setup(name='geoscript',
       version='0.1', 
       description='GeoScript Python',
@@ -10,5 +9,4 @@ setup(name='geoscript',
       author_email='jdeolive@opengeo.org',
       url='http://geoscript.org',
       packages=find_packages(),
-      data_files=[('geotools', geotools_libs)]
      )
