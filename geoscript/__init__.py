f53a2dcbc00bb5c6c83943b1ef8853e09e61a28d
jdeolive <jdeolive@192-168-100-47.tpgi.com.au>
2009-11-02 13:49:40 +1000

    removed auto import of geotools libs into classpath

--- geoscript/__init__.py
+++ geoscript/__init__.py
@@ -1,20 +1,8 @@
-# first thing to do is ensure that the geotools libs are on the classpath
 try:
-  import org.geotools
+   import org.geotools
 except ImportError:
-  # try to find them
-  import sys, os
-  libs = os.path.abspath(os.path.join(__file__, '../../geotools'))
-  if os.path.exists(libs): 
-     # round up all the libs and add them to the classpath
-     for lib in os.listdir(libs):
-       sys.path.append(os.path.join(libs,lib))
-
-  try:
-     import org.geotools
-  except ImportError:
-     print "Error: Could not find GeoTools libraries on classpath."
-     sys.exit(1)
+   print "Error: Could not find GeoTools libraries on classpath."
+   sys.exit(1)
 
 from java.lang import System
 from org.geotools.factory import Hints
