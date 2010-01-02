8cf8bf8553b35009e4063628eca617c87882108b
jdeolive <jdeolive@jdeolive.local>
2010-01-02 15:27:10 -0800

    added runnable scripts, including gt libs in distribution, moved jar directory

--- geoscript/__init__.py
+++ geoscript/__init__.py
@@ -1,16 +1,19 @@
 import sys
+from java.lang import System
+
 try:
    import org.geotools
+   from org.geotools.factory import Hints
+
+   # by default the geotools referenceing Systemtem assumes yx or lat/lon 
+   if not System.getProperty("org.geotools.referencing.forceXY"):
+      System.setProperty("org.geotools.referencing.forceXY", "true")
+
+   if Hints.getSystemDefault(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER):
+      Hints.putSystemDefault(Hints.FORCE_AXIS_ORDER_HONORING, "http")
+
 except ImportError:
-   print "Error: Could not find GeoTools libraries on classpath."
-   sys.exit(1)
+   if not System.getProperty("geoscript.bootstrap"):
+      print "Error: Could not find GeoTools libraries on classpath."
+      sys.exit(1)
 
-from java.lang import System
-from org.geotools.factory import Hints
-
-# by default the geotools referenceing Systemtem assumes yx or lat/lon 
-if not System.getProperty("org.geotools.referencing.forceXY"):
-  System.setProperty("org.geotools.referencing.forceXY", "true")
-
-if Hints.getSystemDefault(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER):
-  Hints.putSystemDefault(Hints.FORCE_AXIS_ORDER_HONORING, "http")
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 from distutils.core import setup
 from setuptools import find_packages
 
+jars = ['jars/%s' % (f) for f in os.listdir('jars')]
 setup(name='geoscript',
       version='0.1', 
       description='GeoScript Python',
@@ -9,4 +10,6 @@ setup(name='geoscript',
       author_email='jdeolive@opengeo.org',
       url='http://geoscript.org',
       packages=find_packages(),
+      data_files=[('jars', jars)],
+      scripts=['bin/geoscript-classpath', 'bin/geoscript']
      )
