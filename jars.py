c568a397a5df0f21b2afbc1434f8520f29f841f1
Justin Deoliveira <jdeolive@Justin-Deoliveiras-MacBook.local>
2010-01-28 21:18:40 -0500

    added build.xml to download necessary jars

--- jars.py
+++ jars.py
@@ -8,17 +8,26 @@ def lookup(arg,dirname,names):
    if arg in names:
      url = os.path.join(dirname,arg)[len(m2repo)+1:]
 
+     print '\t\t<get src="%s/%s" dest="jars" usetimestamp="true"/>' % (ogrepo, url)
+"""
      print 'if [ ! -e target/%s ]; then' % (arg)
      print '\techo "Downloading %s"' % (arg)
      print '\tcurl -G $base/%s -o target/%s' % (url,arg)
      print 'fi'
+"""
 
+"""
 print 'if [ ! -e target ]; then'
 print "\tmkdir target"
 print 'fi'
 
 print "base=%s" % ogrepo
-for jar in os.listdir('.'):
+"""
+print '<project name="jars" default="jars">'
+print '\t<target name="jars">'
+for jar in os.listdir('jars'):
   if os.path.splitext(jar)[1] == ".jar":
      os.path.walk(m2repo,lookup,jar)
 
+print '\t</target>'
+print '</project>'
