b8bed598656f1a30e3b87ee77f5c6b0697fa2354
Justin Deoliveira <jdeolive@columbia.(none)>
2010-03-13 14:25:16 -0500

    refactored raster classes

--- geoscript/raster/worldimage.py
+++ geoscript/raster/worldimage.py
@@ -1,12 +1,10 @@
-import sys
-from geoscript import util
 from geoscript.raster.raster import Raster
-from org.geotools.gce.image import WorldImageReader
+from org.geotools.gce.image import WorldImageFormat
 
 class WorldImage(Raster):
 
-  def __init__(self, file):
-    Raster.__init__(self, WorldImageReader(util.toFile(file)))
+  def __init__(self, file, proj=None):
+    Raster.__init__(self, WorldImageFormat(), file, proj)
 
   def getformat(self):
     return str(Raster.getformat(self) + ' (%s)' % self._reader.getExtension())
