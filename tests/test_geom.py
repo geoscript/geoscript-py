845d9b6fa4d3dc20b42295c3d42c330c35f92410
jdeolive <jdeolive@gmail.com>
2010-06-17 15:53:23 -0600

    added tests for bounds reproject and changed some text assertions for geom wkt

--- tests/test_geom.py
+++ tests/test_geom.py
@@ -35,11 +35,11 @@ class GeomTest(unittest.TestCase):
 
   def testMultiPoint(self):
     mp = geom.MultiPoint([1,2], [3,4])
-    self.assertEqual('MULTIPOINT (1 2, 3 4)', str(mp))
+    self.assertEqual('MULTIPOINT ((1 2), (3 4))', str(mp))
 
   def testMultiPointFromJTS(self):
     mp = geom.MultiPoint(self.gf.createMultiPoint([self.gf.createPoint(Coordinate(1,2)), self.gf.createPoint(Coordinate(3,4))]))
-    self.assertEqual('MULTIPOINT (1 2, 3 4)', str(mp))
+    self.assertEqual('MULTIPOINT ((1 2), (3 4))', str(mp))
 
   def testMultiLineString(self):
     ml = geom.MultiLineString([[1,2],[3,4]], [[5,6],[7,8]])
@@ -61,6 +61,15 @@ class GeomTest(unittest.TestCase):
     b = geom.Bounds(1.0, 2.0, 3.0, 4.0, 'epsg:4326')
     self.assertEqual('(1.0, 2.0, 3.0, 4.0, EPSG:4326)', str(b))
     
+  def testBoundsReproject(self):
+    b = geom.Bounds(-111, 44.7, -110, 44.9, 'epsg:4326')
+    b1 = b.reproject('epsg:26912')
+    self.assertEqual(499999, int(b1.west))
+    self.assertEqual(4949624, int(b1.south))
+    self.assertEqual(579224, int(b1.east))
+    self.assertEqual(4972327, int(b1.north))
+    
+
   def testMultiPolygonFromJTS(self):
     mp = geom.MultiPolygon(self.gf.createMultiPolygon([self.gf.createPolygon(self.gf.createLinearRing([Coordinate(1,2),Coordinate(3,4),Coordinate(5,6),Coordinate(1,2)]),[])]))
     self.assertEqual('MULTIPOLYGON (((1 2, 3 4, 5 6, 1 2)))', str(mp))
