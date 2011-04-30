b75bb80d6c1af04e6aca04720ece6c82b83def6c
jdeolive <jdeolive@gmail.com>
2011-04-30 16:06:26 -0600

    using util.toOutputStream() to create output stream when serializing sld

--- geoscript/style/io/sld.py
+++ geoscript/style/io/sld.py
@@ -1,9 +1,16 @@
 import sys
+from geoscript import util
 from org.geotools.styling import SLDTransformer
 
-def writeSLD(style, format=True):
+def writeSLD(style, out=sys.stdout, format=True):
   tx = SLDTransformer()
   if format: 
     tx.setIndentation(2)
-  tx.transform(style, sys.stdout)
+
+  out = util.toOutputStream(out)
+  try:
+    tx.transform(style, out)
+  finally:
+    if out and out.close:
+       out.close()
   
