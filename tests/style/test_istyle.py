a4c9fcdd0591fce0936be6388dabedd9966ab571
jdeolive <jdeolive@gmail.com>
2011-07-25 21:44:08 -0600

    made istyle check for java.awt.headless

--- tests/style/test_istyle.py
+++ tests/style/test_istyle.py
@@ -4,66 +4,14 @@ from geoscript.style import *
 from geoscript.render import Map
 from geoscript.layer import Shapefile
 
-class InteractiveStyle_Test:
+def points():
+   return Shapefile('work/point.shp')
 
-  def testStrokeSimple(self):
-    self.render(Stroke(), self.lines(), "simple stroke") 
+def lines():
+   return Shapefile('work/line.shp')
 
-  def testStrokeHatch(self):
-    self.render(Stroke() + Stroke().hatch('vertline').zindex(1), self.lines(), "stroke with hatch") 
-
-  def testLabelSimple(self):
-    self.render(Stroke() + Label('name'), self.lines(), "simple label") 
-
-  def testLabelLinear(self):
-    self.render(Stroke() + Label('name').linear(), self.lines(), "simple linear label")
-
-  def testLabelLinearOffset(self):
-    self.render(Stroke() + Label('name').linear(25), self.lines(), "simple label with line placement") 
-
-  def testLabelCurved(self):
-    self.render(Stroke() + Label('name').linear(follow=True), self.lines(), "curved label") 
-
-  def testLabelCurvedOffset(self):
-    self.render(Stroke() + Label('name').linear(offset=25, follow=True), self.lines(), "curved label with offset") 
-
-  def testLabelFont(self):
-    self.render(Stroke() + Label('name').font('bold 16px "Times New Roman"'), self.lines(), "label with font") 
-
-  def testPolySimple(self):
-    self.render(Fill(), self.polygons(), "simple polygon")
-
-  def testPolyWithNoFill(self):
-    self.render(Stroke(), self.polygons(), "polygon no fill")
-
-  def testPolyLabel(self):
-    self.render(Fill() + Label('name'), self.polygons(), "polygon with label")
-
-  """
-  def testPolyLabelWithHalo(self):
-    self.render(Fill() + Label('name').font('16px "Times New Roman"').halo(), self.polygons(),  "polygon label with halo") 
-
-  """
-
-  def testPolyHatch(self):
-    self.render(Fill().hatch('backslash'), self.polygons(), "polygon with hatch")
-  def testIcon(self):
-    self.render(Icon('data/hospital16.png'), self.points(), 'simple icon')
-
-  def render(self, style, layer, title):
-    w = Worker(layers=layer, styles=style, title=title)
-    w.start()
-    time.sleep(2)
-    w.dispose()
-    
-  def points(self):
-    return Shapefile('work/point.shp')
-
-  def lines(self):
-    return Shapefile('work/line.shp')
-
-  def polygons(self):
-    return Shapefile('work/polygon.shp')
+def polygons():
+   return Shapefile('work/polygon.shp')
 
 class Worker(threading.Thread):
 
@@ -78,3 +26,33 @@ class Worker(threading.Thread):
    def dispose(self):
      self.renderer.window.dispose()
      self.map.dispose()
+
+def render(style, layer, title):
+   w = Worker(layers=layer, styles=style, title=title)
+   w.start()
+   time.sleep(2)
+   w.dispose()
+
+tests = [
+  (Stroke(), lines, "simple stroke"), 
+  (Stroke()+Stroke().hatch('vertline').zindex(1), lines, "stroke with hatch"), 
+  (Stroke()+Label('name'), lines, "simple label"),
+  (Stroke()+Label('name').linear(), lines, "simple linear label"),
+  (Stroke()+Label('name').linear(25), lines,"simple label with line placement"),
+  (Stroke()+Label('name').linear(follow=True), lines, "curved label"),
+  (Stroke()+Label('name').linear(offset=25, follow=True), lines, "curved label with offset"),
+  (Stroke()+Label('name').font('bold 16px "Times New Roman"'), lines, "label with font"), 
+  (Fill(), polygons, "simple polygon"),
+  (Stroke(), polygons, "polygon no fill"),
+  (Fill()+Label('name'), polygons, "polygon with label"),
+  #  (Fill() + Label('name').font('16px "Times New Roman"').halo(), polygons,  "polygon label with halo") ,
+  (Fill().hatch('backslash'), polygons, "polygon with hatch"),
+  (Icon('data/hospital16.png'), points, 'simple icon')
+]
+
+def test():
+   from java.lang import System 
+   if not System.getProperty("java.awt.headless"):
+     for t in tests:
+       yield render, t[0], t[1](), t[2]
+
