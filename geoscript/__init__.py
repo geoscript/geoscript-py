b2d3ff197ea61394dae04f90c6103417e66cd6d0
jdeolive <jdeolive@gmail.com>
2011-09-10 20:37:19 -0600

    added hint to make wkt matching more lax in order to more easily match random wkt to epsg code

--- geoscript/__init__.py
+++ geoscript/__init__.py
@@ -12,6 +12,11 @@ try:
    if Hints.getSystemDefault(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER):
       Hints.putSystemDefault(Hints.FORCE_AXIS_ORDER_HONORING, "http")
 
+   # set the matching tolerance in the referencing model, this makes it easier
+   # to match projection definitions that don't have epsg codes to an existing
+   # code
+   Hints.putSystemDefault(Hints.COMPARISON_TOLERANCE, 1e-9);
+
 except ImportError:
    if not System.getProperty("geoscript.bootstrap"):
       print "Error: Could not find GeoTools libraries on classpath."
