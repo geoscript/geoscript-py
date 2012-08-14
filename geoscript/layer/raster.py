b0563214e1fc504151994c950d2938a63cc30580
jdeolive <jdeolive@opengeo.org>
2012-08-13 20:26:55 -0600

    more raster goodness, creation of multi band rasters from scratch, operations, histogram, extrema, etc...

--- geoscript/layer/raster.py
+++ geoscript/layer/raster.py
@@ -1,16 +1,20 @@
 import os, sys
 from jarray import array
+from java.awt.image import DataBuffer
+from javax.media.jai import RasterFactory
 from geoscript import core, util
 from geoscript.proj import Projection
-from geoscript.geom import Bounds
+from geoscript.geom import Bounds, Point
 from geoscript.feature import Feature
 from geoscript.layer.band import Band
 from org.opengis.parameter import GeneralParameterValue
+from org.opengis.referencing.datum import PixelInCell
 from org.geotools.factory import Hints
+from org.geotools.geometry import DirectPosition2D
 from org.geotools.parameter import Parameter
-from org.geotools.coverage import CoverageFactoryFinder
+from org.geotools.coverage import CoverageFactoryFinder, GridSampleDimension
 from org.geotools.coverage.grid import GridCoverage2D, GridGeometry2D
-from org.geotools.coverage.grid import GridEnvelope2D
+from org.geotools.coverage.grid import GridEnvelope2D, GridCoordinates2D
 from org.geotools.coverage.grid.io import AbstractGridFormat
 from org.geotools.coverage.processing import CoverageProcessor
 from org.geotools.process.raster.gs import ScaleCoverage, CropCoverage
@@ -20,9 +24,38 @@ from org.geotools.process.raster.gs import RasterAsPointCollectionProcess
 class Raster(object):
 
   @staticmethod
-  def create(data, bounds):
+  def create(data, bounds, nband=1, bands=None, dataType=DataBuffer.TYPE_FLOAT):
+    """
+    Creates a new Raster. *data* may be specified as a two dimensional list or
+    array. It may be also specified as 
+    """
     factory = CoverageFactoryFinder.getGridCoverageFactory(None)
-    coverage = factory.create('raster', data, bounds)
+
+    if not bands:
+      bands = [Band('band%d' % i) for i in range(nband)]
+      
+    nband = len(bands)
+
+    if isinstance(data, list):
+      # copy the data into a writeable raster
+      h, w = len(data), len(data[0])
+      wr = RasterFactory.createBandedRaster(dataType, w, h, nband, None)
+
+      for j in range(len(data)):
+        for i in range(len(data[j])):
+          if nband > 1:
+            for b in range(nband):
+              wr.setSample(i, j, b, data[j][i][b])
+          else:
+            wr.setSample(i, j, 0, data[j][i])
+
+      data = wr
+
+    if nband > 1:
+      coverage = factory.create('raster', data, bounds, [b._dim for b in bands])
+    else:
+      coverage = factory.create('raster', data, bounds)
+
     return Raster(None, coverage=coverage)
 
   def __init__(self, format, file=None, proj=None, coverage=None, reader=None):
@@ -77,7 +110,7 @@ class Raster(object):
   def getbands(self):
     bands = []    
     for i in range(self._coverage.getNumSampleDimensions()):
-      bands.append(Band(self._coverage.getSampleDimension(i)))
+      bands.append(Band(dim=self._coverage.getSampleDimension(i)))
 
     return bands
 
@@ -100,6 +133,48 @@ class Raster(object):
 
   data = property(getdata, None)
 
+  def eval(self, point=None, pixel=None):
+     """
+     Returns the value of the raster at the specified location.
+  
+     The location can be specified with the *point* parameter (world space) or
+     with the *pixel* parameter (image space).
+     """
+     if point:
+       p = Point(point)
+     elif pixel:
+       p = self.point(pixel)
+     else:
+       p = self.point((0,0))
+
+     return list(self._coverage.evaluate(DirectPosition2D(p.x, p.y)))
+
+  def point(self, pixel):
+     """
+     Maps a coordinate from world space to pixel space returning the result as a
+     :class:`Point <geoscript.geom.point.Point>` object.
+
+     The *pixel* parameter may be specified as an x/y ``tuple`` or ``list`` or
+     as a :class:`Point <geoscript.geom.point.Point>` object.
+     """
+     p = Point(pixel)
+     gg = self._coverage.getGridGeometry()
+     dp = gg.gridToWorld(GridCoordinates2D(int(p.x), int(p.y)))
+     return Point(dp.x, dp.y)
+  
+  def pixel(self, point):
+     """
+     Maps a coordinate from pixel space to world space returning the result as a
+     :class:`Point <geoscript.geom.point.Point>` object.
+
+     The *point* parameter may be specified as an x/y ``tuple`` or ``list`` or
+     as a :class:`Point <geoscript.geom.point.Point>` object.
+     """
+     p = Point(point)
+     gg = self._coverage.getGridGeometry()
+     dp = gg.worldToGrid(DirectPosition2D(p.x, p.y))
+     return (dp.x, dp.y)
+    
   def resample(self, bbox=None, rect=None, size=None):
      """
      Resamples this raster returning a new raster. The *bbox* argument specifies
@@ -152,6 +227,44 @@ class Raster(object):
     cc = CropCoverage()
     result = cc.execute(self._coverage, geom, None)
     return Raster(self._format, coverage=result, reader=self._reader)
+
+  def histogram(self, low=None, high=None, nbins=None):
+    """
+    Computes a histogram for the raster.  
+
+    *low* specifies the lowest (inclusive) pixel value to include and *high* 
+    specifies the highest (exclusive) pixel value to include.
+
+    *nbins* specifies the number of bins/buckets of the resulting histogram.
+
+    This method returns a :class:`Histogram <geoscript.layer.raster.Histogram>`
+    instance.
+    """
+    nb = len(self.bands)
+
+    params = {'Source': self._coverage}
+    if low: 
+      low = low if isinstance(low, (tuple, list)) else [low]*nb
+      params['lowValue'] = array(low, 'd')
+    if high:
+      high = high if isinstance(high, (tuple, list)) else [high]*nb
+      params['highValue'] = array(high, 'd')
+    if nbins:
+      nbins = nbins if isinstance(nbins, (tuple, list)) else [nbins]*nb
+      params['numBins'] = array(nbins, 'i')
+
+    h = self._op('Histogram', **params).getProperty('histogram') 
+    return Histogram(h)
+
+  def extrema(self):
+    """
+    Computes the min and max values of each band in the raster.
+
+    This method returns a (min,max) ``tuple`` in which each element is a 
+    ``list`` consisting of the min or max value corresponding to each band. 
+    """
+    result = self._op('Extrema', Source=self._coverage).getProperty('extrema')
+    return tuple([list(x) for x in result])
 
   def features(self):
     """
@@ -228,3 +341,22 @@ class Raster(object):
 
 core.registerTypeMapping(GridCoverage2D, Raster, lambda x: Raster(coverage=x))
 core.registerTypeUnmapping(Raster, GridCoverage2D, lambda x: x._coverage)
+
+class Histogram(object):
+
+  def __init__(self, histo):
+    self._histo = histo
+
+  def bin(self, i, band=0):
+    h = self._histo
+    if i < h.getNumBins(band):
+      return (h.getBinLowValue(band, i), h.getBinLowValue(band, i+1))
+
+  def count(self, i, band=0):
+    return self._histo.getBinSize(band, i)
+
+  def __len__(self):
+    return self._histo.getNumBins(0)
+
+  def __getitem__(self, key):
+    return self.count(key)
