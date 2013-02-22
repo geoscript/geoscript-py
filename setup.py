a7ae6bacd2fa78a73e0ff90967f4e745ffce71dc
jdeolive <jdeolive@opengeo.org>
2013-02-22 09:22:14 -0700

    upgrading minor version number

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class DocCmd(Command):
           self.distribution.dist_files.append(file)
 
 
-ver='1.2-20121010'
+ver='1.3.x'
 jars = ['jars/%s' % (f) for f in os.listdir('jars')]
 setup(cmdclass={'src': SrcCmd, 'doc': DocCmd}, 
       name='geoscript',
