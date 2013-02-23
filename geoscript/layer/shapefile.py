30c28a4fb17830cc04a198d5c075f701d31e1a87
jdeolive <jdeolive@opengeo.org>
2013-02-23 09:51:41 -0700

    adding schema argument to Shapefile constructor

--- geoscript/layer/shapefile.py
+++ geoscript/layer/shapefile.py
@@ -12,13 +12,16 @@ class Shapefile(Layer):
   A subclass of :class:`Layer <geoscript.layer.layer.Layer>` for the Shapefile format.
 
   *file* is the path to the Shapefile as a ``str``.
+
+  *schema* is optional and only relevent when creating a new Shapefile. It 
+  specifies the schema for the new file.
   """
-  def __init__(self, file):
+  def __init__(self, file, schema=None):
     f = util.toFile(file) 
     name = path.splitext(path.basename(file))[0]
 
     from geoscript.workspace import Directory
-    Layer.__init__(self, name, Directory(f.canonicalFile.parent))
+    Layer.__init__(self, name, Directory(f.canonicalFile.parent), schema=schema)
 
   def getfile(self):
     return self._source.dataStore.info.source.toURL().file
