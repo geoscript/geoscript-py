29c705961517d54e3cf0164c398774f1adf4a998
jdeolive <jdeolive@boundlessgeo.com>
2014-02-08 10:33:12 -0700

    Setting version numbers for 1.2.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class DocCmd(Command):
           self.distribution.dist_files.append(file)
 
 
-ver='1.2-20130322'
+ver='1.2.1'
 jars = ['jars/%s' % (f) for f in os.listdir('jars')]
 setup(cmdclass={'src': SrcCmd, 'doc': DocCmd}, 
       name='geoscript',
