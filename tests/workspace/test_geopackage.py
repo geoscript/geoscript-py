bd13185692d351696761c05bfe85c8ddbc141fa2
jdeolive <jdeolive@gmail.com>
2015-06-17 09:10:58 -0600

    Some cleanup for geopackage test.

--- tests/workspace/test_geopackage.py
+++ tests/workspace/test_geopackage.py
@@ -5,4 +5,13 @@ from geoscript.workspace import GeoPackage
 class GeoPackageWorkspace_Test(WorkspaceTest):
 
   def setUp(self):
-    self.ws = GeoPackage('data.gpkg')
+    self.ws = GeoPackage('work/data.gpkg')
+    self.remove('widgets')
+    self.remove('widgets2')
+    self.remove('states2')
+
+  def remove(self, layer):
+    try:
+      self.ws.remove(layer)
+    except:
+      pass
