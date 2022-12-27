a2cbb8ac32cc315b98ac7d650de613c9e830efe7
Jared Erickson <jared.erickson@gmail.com>
2022-12-27 10:45:37 -0800

    Release 1.11.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class DocCmd(Command):
           self.distribution.dist_files.append(file)
 
 
-ver='1.10.0'
+ver='1.11.0'
 jars = ['jars/%s' % (f) for f in os.listdir('jars')]
 setup(cmdclass={'src': SrcCmd, 'doc': DocCmd}, 
       name='geoscript',
